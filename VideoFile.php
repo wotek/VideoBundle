bf506b5a04fc74b9b8ca4d75f1992d3ef0543803
wotek <wojtek@kropka.net>
2013-08-26 08:29:10 +0200

    Save.

---
+++ VideoFile.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wtk\VideoBundle;
+
+use Symfony\Component\HttpFoundation\File\File;
+
+class VideoFile extends File
+{
+  /**
+   * @var string
+   */
+  protected $title;
+
+  /**
+   * @var string
+   */
+  protected $description;
+
+  /**
+   * @param string $title
+   */
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  }
+
+  /**
+   * @return string
+   */
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  /**
+   * @param string $description
+   */
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDescription()
+  {
+    return $this->description;
+  }
+}
