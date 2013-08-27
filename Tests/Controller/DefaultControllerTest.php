eaf77015401224fe84a1e6df1e702537f1e110a6
wotek <wojtek@kropka.net>
2013-08-27 13:10:48 +0200

    unit tests init

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -2,16 +2,10 @@
 
 namespace Wtk\VideoBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends \PHPUnit_TestCase
 {
-    public function testIndex()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
-    }
+  public function testFoo()
+  {
+    $this->assertTrue(true);
+  }
 }
